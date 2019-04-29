dd363a54dcfb3d2e59fb285a804e25566e5774cb
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2019-04-29 14:27:27 +0200

    asdsadsa

--- styles/theme.js
+++ styles/theme.js
@@ -3,6 +3,23 @@ import { createMuiTheme } from "@material-ui/core/styles"
 const theme = createMuiTheme({
     typography: {
         useNextVariants: true,
+        fontFamily: [
+            "Comic Sans",
+        ],
+    },
+    palette: {
+        primary: {
+        // light: will be calculated from palette.primary.main,
+            main: "#ff4400",
+        // dark: will be calculated from palette.primary.main,
+        // contrastText: will be calculated to contrast with palette.primary.main
+        },
+        secondary: {
+            light: "#0066ff",
+            main: "#0044ff",
+            // dark: will be calculated from palette.secondary.main,
+            contrastText: "#ffcc00",
+        },
     },
 })
 
