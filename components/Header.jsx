b510f3297048190edabc53bf2b9d195732bcf09a
Dorine Lambinet <dorine.lambinet@gmail.com>
2019-04-29 21:27:56 +0200

    Header et typographie
    J'ai ajouté un composant "Header" et j'ai changé la typographie pour la Lato.

---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography,Button } from "@material-ui/core"
+
+
+export default props =>
+
+      <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h4" color="inherit">
+            Leximpact
+          </Typography>
+          <Button color="inherit">Vos Retours !</Button>
+        </Toolbar>
+      </AppBar>
+  
