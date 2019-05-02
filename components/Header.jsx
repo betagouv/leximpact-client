7d11b48f4c4f82603fbe6311eea8c4fb1e463d68
Dorine Lambinet <dorine.lambinet@gmail.com>
2019-05-02 15:35:58 +0200

    Install Lora typographie
    Installation de la typographie Lora et début de mise en page de l'article 197

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography,Button } from "@material-ui/core"
 import SvgIcon from '@material-ui/core/SvgIcon';
-
+//import MuiThemeProvider from '@material-ui/styles/MuiThemeProvider';
+//import getPageContext from '../lib/getPageContext'
 
 export default props =>
 
-      <AppBar position="static">
-        <Toolbar>
-          
+      <AppBar  position="static">
+        <Toolbar>         
           <Typography variant="h4" color="inherit">
             Leximpact
           </Typography>
           
               
-          <Button color="inherit">Vos Retours !</Button>
+          <Button color="inherit">
+          	Vos Retours !
+          </Button>
           
         </Toolbar>
       </AppBar>
   
 
- 
