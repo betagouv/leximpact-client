ddf95b28290d9693026c0c726b5772186eebd731
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2019-05-03 15:23:49 +0200

    Fix CSS

--- app/next.config.js
+++ app/next.config.js
@@ -1,43 +1,10 @@
-const path = require("path")
-const glob = require("glob")
+const withSass = require("@zeit/next-sass")
 
-module.exports = {
-    webpack: (config) => {
-        config.module.rules.push(
-            {
-                test: /\.(css|scss)/,
-                loader: "emit-file-loader",
-                options: {
-                    name: "dist/[path][name].[ext]",
-                },
-            }
-            ,
-            {
-                test: /\.css$/,
-                use: ["babel-loader", "raw-loader", "postcss-loader"],
-            }
-            ,
-            {
-                test: /\.s(a|c)ss$/,
-                use: ["babel-loader", "raw-loader", "postcss-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            includePaths: ["styles", "node_modules"]
-                                .map((d) => path.join(__dirname, d))
-                                .map((g) => glob.sync(g))
-                                .reduce((a, c) => a.concat(c), []),
-                        },
-                    },
-                ],
-            }
-        )
-        return config
-    },
+module.exports = withSass({
     exportPathMap() {
         return {
             "/": { page: "/" },
         }
     },
     distDir: "build",
-}
+})
