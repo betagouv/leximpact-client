38abd668f9ed1a8446a1894df5900af939bf1c51
Augustin Wenger <magemax@yahoo.com>
2019-06-12 14:35:33 +0200

    Mise en place d'une image de background

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withTranspileModules = require("next-transpile-modules")
 const withTypescript = require("@zeit/next-typescript")
 const withSass = require("@zeit/next-sass")
+const withImages = require('next-images');
+
 const _ = require("lodash")
 
 const config = {
@@ -13,4 +15,4 @@ const config = {
     transpileModules: ["@iconify/react"],
 }
 
-module.exports = _.flow([withTranspileModules, withTypescript, withSass])(config)
+module.exports = _.flow([withTranspileModules, withTypescript, withSass,withImages])(config)
