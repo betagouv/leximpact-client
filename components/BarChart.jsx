2221a3c82550ac68752b9864c7930da28a3a8cd2
Augustin Wenger <magemax@yahoo.com>
2019-06-13 10:47:40 +0200

    style du graphe exemple se rapproche du layout désiré

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -5,97 +5,6 @@ import "./chart.scss"
 import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 
-var config= {
-    keys: [
-        "hot dog",
-        "burger",
-        "sandwich",
-        "kebab",
-        "fries",
-        "donut"
-    ],
-    margin: {
-        "top": 50,
-        "right": 130,
-        "bottom": 50,
-        "left": 60
-    },
-    defs: [
-        {
-            "id": "dots",
-            "type": "patternDots",
-            "background": "inherit",
-            "color": "#38bcb2",
-            "size": 4,
-            "padding": 1,
-            "stagger": true
-        },
-        {
-            "id": "lines",
-            "type": "patternLines",
-            "background": "inherit",
-            "color": "#eed312",
-            "rotation": -45,
-            "lineWidth": 6,
-            "spacing": 10
-        }
-    ],
-    fill: [
-        {
-            "match": {
-                "id": "fries"
-            },
-            "id": "dots"
-        },
-        {
-            "match": {
-                "id": "sandwich"
-            },
-            "id": "lines"
-        }
-    ],
-    axisBottom: {
-        "tickSize": 5,
-        "tickPadding": 5,
-        "tickRotation": 0,
-        "legend": "country",
-        "legendPosition": "middle",
-        "legendOffset": 32
-    },
-    axisLeft: {
-        "tickSize": 5,
-        "tickPadding": 5,
-        "tickRotation": 0,
-        "legend": "food",
-        "legendPosition": "middle",
-        "legendOffset": -40
-    },
-    legends: [
-        {
-            "dataFrom": "keys",
-            "anchor": "bottom-right",
-            "direction": "column",
-            "justify": false,
-            "translateX": 120,
-            "translateY": 0,
-            "itemsSpacing": 2,
-            "itemWidth": 100,
-            "itemHeight": 20,
-            "itemDirection": "left-to-right",
-            "itemOpacity": 0.85,
-            "symbolSize": 20,
-            "effects": [
-                {
-                    "on": "hover",
-                    "style": {
-                        "itemOpacity": 1
-                    }
-                }
-            ]
-        }
-    ]
-}
-
 var data=[
     {
         "country": "AD",
@@ -207,11 +116,77 @@ var data=[
 class BarChart extends React.Component {
     render() {
         return (
-        <Card>
-            <CardContent>
-            <div class="chart">
-                Coucou
-                <ResponsiveBar
+                <Card>
+                    <CardContent>
+                    <div class="chart">
+                    <ResponsiveBar
+                data={data}
+                keys={[ 'hot dog', 'burger', 'sandwich', 'kebab', 'fries', 'donut' ]}
+                indexBy="country"
+                margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+            padding={0.15}
+            innerPadding={3}
+            maxValue={1000}
+            layout="horizontal"
+            colors={{ scheme: 'nivo' }}
+            defs={[
+                {
+                    id: 'dots',
+                    type: 'patternDots',
+                    background: 'inherit',
+                    color: '#38bcb2',
+                    size: 4,
+                    padding: 1,
+                    stagger: true
+                },
+                {
+                    id: 'lines',
+                    type: 'patternLines',
+                    background: 'inherit',
+                    color: '#eed312',
+                    rotation: -45,
+                    lineWidth: 6,
+                    spacing: 10
+                }
+            ]}
+            fill={[
+                {
+                    match: {
+                        id: 'fries'
+                    },
+                    id: 'dots'
+                },
+                {
+                    match: {
+                        id: 'sandwich'
+                    },
+                    id: 'lines'
+                }
+            ]}
+            borderRadius={4}
+            borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 7,
+                tickRotation: 0,
+                legend: "Volume des recettes de l'IR en Md€",
+                legendPosition: 'middle',
+                legendOffset: 41
+            }}
+            axisLeft={null}
+            enableGridX={true}
+            enableGridY={false}
+            labelSkipWidth={12}
+            labelSkipHeight={12}
+            labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+            legends={[]}
+            animate={true}
+            motionStiffness={90}
+            motionDamping={15}
+        />
+              {/*  <ResponsiveBar
                     data={data}
                     keys={config.keys}
                     indexBy="country"
@@ -234,7 +209,7 @@ class BarChart extends React.Component {
                     motionDamping={15}
                     legends={config.legends}
                 />
-                coucou
+              coucou*/}
             </div>
         </CardContent>
     </Card>
