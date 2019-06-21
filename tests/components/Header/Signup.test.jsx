73abe766f34c21dbf4bc10a655725189eaa859ae
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2019-06-21 11:34:31 +0200

    Add Signup component

---
+++ tests/components/Header/Signup.test.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable fp/no-nil */
+
+import { create } from "react-test-renderer"
+import Signup from "components/Header/Signup"
+
+test("Signup renders correctly", () => {
+    const renderer = create(<Signup />)
+    const instance = renderer.root
+    const [text] = instance.findByType("p").children
+
+    expect(text).toEqual("S’enregistrer")
+})
