397533f088ff4dda757f3bbe15eaef93c9f53e6d
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2019-06-30 23:31:59 +0200

    Fix jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 const jestConfig = {
     moduleFileExtensions: ["js", "jsx"],
+    moduleNameMapper: { "\\.(scss)$": "identity-obj-proxy" },
     setupFilesAfterEnv: ["<rootDir>tests/enzyme.config.js"],
+    testEnvironment: "node",
     testMatch: ["**/tests/**/*.test.js?(x)"],
     testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/"],
 }
