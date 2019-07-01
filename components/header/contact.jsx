f2924eaa759a667479c398f74cdc7fd1698cf608
Augustin Wenger <magemax@yahoo.com>
2019-07-01 19:09:37 +0200

    Ajout du bouton "VOS RETOURS!!!!"

---
+++ components/header/contact.jsx
@@ -0,0 +1,13 @@
+import Button from "@material-ui/core/Button"
+
+function Contact() {
+    return (    
+        <a href="mailto:leximpact@openfisca.org" style={{color: 'white', textDecoration: 'none'}}>
+            <Button color="inherit">
+                <p>Vos Retours !</p>
+            </Button>
+        </a>
+    )
+}
+
+export default Contact
