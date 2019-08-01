d917d8422809e3534e7248a07a2842e0dd1254a0
sixertoy <sixertoy.github@gmail.com>
2019-08-01 17:42:44 +0200

    🚧 ajout des regles eslint pour les composants du reformeur

--- components/reformeur/article-header.jsx
+++ components/reformeur/article-header.jsx
@@ -1,50 +1,68 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { withStyles } from "@material-ui/core/styles"
-import { AppBar, Toolbar, Typography } from "@material-ui/core"
+/* eslint
+    indent: [2, 2],
+    semi: [2, "always"],
+    react/jsx-indent: [2, 2],
+    react/jsx-indent-props: [2, 2],
+    max-nested-callbacks: [2, { "max": 4 }],
+    react/jsx-closing-bracket-location: [2, {
+        "nonEmpty": false,
+        "selfClosing": false
+    }],
+    "jsx-a11y/anchor-is-valid": [2, {
+      "components": ["Link"],
+      "specialLink": ["hrefLeft", "hrefRight"]
+    }],
+    import/order: [2, {
+      newlines-between: "always",
+      groups: ["builtin", "external", "parent", "sibling", "index"]
+    }]
+*/
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 const styles = {
-    root: {
-        flexGrow: 1,
-        paddingTop: "0px",
-    },
+  root: {
+    flexGrow: 1,
+    paddingTop: "0px",
+  },
 
-    typo1: {
-        fontWeight: "bold",
-        textTransform: "uppercase",
-        color: "#6C6C6C",
-    },
+  typo1: {
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    color: "#6C6C6C",
+  },
 
-    typo2: {
-        color: "#6C6C6C",
-        paddingLeft: "6px",
-    },
-}
+  typo2: {
+    color: "#6C6C6C",
+    paddingLeft: "6px",
+  },
+};
 
 function SimpleAppBar(props) {
-    const { classes } = props
+  const { classes } = props;
 
-    return (
-        <div className={classes.root}>
-            <AppBar
-                position="relative"
-                style={{ background: "#FFFFFF", boxShadow: "none" }}
-            >
-                <Toolbar>
-                    <Typography className={classes.typo1} variant="h4" color="inherit">
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="relative"
+        style={{ background: "#FFFFFF", boxShadow: "none" }}>
+        <Toolbar>
+          <Typography className={classes.typo1} variant="h4" color="inherit">
             Tranches / décote
-                    </Typography>
-                    <Typography className={classes.typo2} variant="body2" color="inherit">
+          </Typography>
+          <Typography className={classes.typo2} variant="body2" color="inherit">
             - Article 197 du CGI
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 SimpleAppBar.propTypes = {
-    classes: PropTypes.shape().isRequired,
-}
+  classes: PropTypes.shape().isRequired,
+};
 
-export default withStyles(styles)(SimpleAppBar)
+export default withStyles(styles)(SimpleAppBar);
--- components/reformeur/impact/index.jsx
+++ components/reformeur/impact/index.jsx
@@ -1,63 +1,83 @@
-import { Component } from "react"
-import { Grid } from "@material-ui/core"
-import SimpleCard from "./simple-card"
-import CarteEtat from "./carte-etat"
+/* eslint
+    indent: [2, 2],
+    semi: [2, "always"],
+    react/jsx-indent: [2, 2],
+    react/jsx-indent-props: [2, 2],
+    max-nested-callbacks: [2, { "max": 4 }],
+    react/jsx-closing-bracket-location: [2, {
+        "nonEmpty": false,
+        "selfClosing": false
+    }],
+    "jsx-a11y/anchor-is-valid": [2, {
+      "components": ["Link"],
+      "specialLink": ["hrefLeft", "hrefRight"]
+    }],
+    import/order: [2, {
+      newlines-between: "always",
+      groups: ["builtin", "external", "parent", "sibling", "index"]
+    }]
+*/
+import { Component } from "react";
+import { Grid } from "@material-ui/core";
+
+import SimpleCard from "./simple-card";
+import CarteEtat from "./carte-etat";
 
 class Impact extends Component {
-    constructor(props) {
-        super(props)
-        this.handleClick = this.handleClick.bind(this)
-        this.handleChangeRevenu = this.handleChangeRevenu.bind(this)
-        this.handleChangeOutreMer = this.handleChangeOutreMer.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChangeRevenu = this.handleChangeRevenu.bind(this);
+    this.handleChangeOutreMer = this.handleChangeOutreMer.bind(this);
+  }
 
-    handleClick(e) {
-        this.props.onClick(e)
-    }
+  handleClick(e) {
+    this.props.onClick(e);
+  }
 
-    handleChangeRevenu(i, e) {
-        this.props.onRevenuChange(i, e.target.value)
-    }
+  handleChangeRevenu(i, e) {
+    this.props.onRevenuChange(i, e.target.value);
+  }
 
-    handleChangeOutreMer(i, v) {
-        this.props.onOutreMerChange(i, v)
-    }
+  handleChangeOutreMer(i, v) {
+    this.props.onOutreMerChange(i, v);
+  }
 
-    render() {
+  render() {
     // include should be false to remove the graph of recettes
-        const includepopulation = false
-        const { loading } = this.props
-        const { res_brut } = this.props
-        const { total_pop } = this.props
-        return (
-            <Grid container spacing={24}>
-                {this.props.cas_types.map((ct, i) => (
-                    <Grid item key={`grid${i}`} xs={6} sm={12} md={6} lg={4} xl={3}>
-                        <SimpleCard
-                            key={`card${i}`}
-                            loading={loading}
-                            onChange={this.handleChangeRevenu}
-                            onOutreMerChange={this.handleChangeOutreMer}
-                            index={i}
-                            desc_cas_type={ct}
-                            impots_avant={res_brut.avant[i]}
-                            impots_apres={res_brut.apres[i]}
-                        />
-                    </Grid>
-                ))}
-                <Grid item key="stateBudget" xs={12} sm={12} md={12} lg={12} xl={12}>
-                    {includepopulation ? (
-                        <CarteEtat resultat={total_pop} onClick={this.handleClick} />
-                    ) : (
-                        <div />
-                    )}
-                </Grid>
-            </Grid>
-        )
-    }
+    const includepopulation = false;
+    const { loading } = this.props;
+    const { res_brut } = this.props;
+    const { total_pop } = this.props;
+    return (
+      <Grid container spacing={24}>
+        {this.props.cas_types.map((ct, i) => (
+          <Grid item key={`grid${i}`} xs={6} sm={12} md={6} lg={4} xl={3}>
+            <SimpleCard
+              key={`card${i}`}
+              loading={loading}
+              onChange={this.handleChangeRevenu}
+              onOutreMerChange={this.handleChangeOutreMer}
+              index={i}
+              desc_cas_type={ct}
+              impots_avant={res_brut.avant[i]}
+              impots_apres={res_brut.apres[i]}
+            />
+          </Grid>
+        ))}
+        <Grid item key="stateBudget" xs={12} sm={12} md={12} lg={12} xl={12}>
+          {includepopulation ? (
+            <CarteEtat resultat={total_pop} onClick={this.handleClick} />
+          ) : (
+            <div />
+          )}
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default Impact
+export default Impact;
 
 /*
 render(
