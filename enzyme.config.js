54e0cfa5e27c52c62078fe99d590070f9241d9d0
sixertoy <sixertoy.github@gmail.com>
2019-08-20 09:54:54 +0200

    🚧 update eslint configuration files, clean packages

--- enzyme.config.js
+++ enzyme.config.js
@@ -8,7 +8,9 @@
       groups: ["builtin", "external", "parent", "sibling", "index"]
     }]
 */
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { configure } from "enzyme";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
