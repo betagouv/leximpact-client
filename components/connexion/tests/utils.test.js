cce495fd052b329d5d64d1491a7b3cbe91e1c03e
Augustin Wenger <magemax@yahoo.com>
2019-09-12 13:08:18 +0200

    Another test to match acctually

--- components/connexion/tests/utils.test.js
+++ components/connexion/tests/utils.test.js
@@ -98,7 +98,7 @@ describe("components | connexion | utils", () => {
     it("retourne les noms de domaine pour un collaborateur", () => {
       const given = "collab";
       const result = updateDomainsWhenRoleChange(given);
-      const expected = ["@clb-an.fr", "@clb-dep.fr"];
+      const expected = ["@clb-an.fr", "@clb-dep.fr", "@assemblee-nationale.fr", "@groupe-udiagir.fr", "@an-en-marche.fr", "@gsan.org"];
       expect(result).toStrictEqual(expected);
     });
 
