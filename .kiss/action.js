4a077a351f234a8a81f23bdbc6683021398f88f9
sixertoy <sixertoy.github@gmail.com>
2019-09-18 08:29:53 +0200

    📚 ajout de la documentation redux

---
+++ .kiss/action.js
@@ -0,0 +1,10 @@
+export const jaiCliqueSurLeBouton = unParametreOptionnel => ({
+  type: "quandJaiCliqueSurLeBouton",
+  unParametreOptionnel,
+});
+
+// export const loadingStart = () => ({
+//   type: "onLoadingStart",
+// });
+
+export default jaiCliqueSurLeBouton;
