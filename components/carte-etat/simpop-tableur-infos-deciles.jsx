9179517148f6c9443b2e9109a63c8a1bffa62dab
sandcha <sandcha@users.noreply.github.com>
2019-09-23 17:32:28 +0200

    Ajoute les colonnes plf et réforme au tableau de la carte état

--- components/carte-etat/simpop-tableur-infos-deciles.jsx
+++ components/carte-etat/simpop-tableur-infos-deciles.jsx
@@ -19,8 +19,11 @@ let id = 0;
 function createData(
   decile,
   salaire,
+  impactmoyenfoyer_plf,
+  impactmoyenfoyer_reforme,
   impotmoyenfoyer,
-  impactmoyenfoyer,
+  impotmoyenfoyer_plf,
+  impotmoyenfoyer_reforme,
   recettesetat,
 ) {
   id += 1;
@@ -28,30 +31,34 @@ function createData(
     id,
     decile,
     salaire,
+    impactmoyenfoyer_plf,
+    impactmoyenfoyer_reforme,
     impotmoyenfoyer,
-    impactmoyenfoyer,
+    impotmoyenfoyer_plf,
+    impotmoyenfoyer_reforme,
     recettesetat,
   };
 }
 
+// #décile, salaire net/mois, impact foyer (plf, réforme), impôt moyen foyer (existant, plf, réforme), total recettes état
 const rows = [
-  createData("Décile n°1", "1 135€/mois", 6.0, 24, 4.0),
-  createData("Décile n°2", "1 455€/mois", 9.0, 37, 4.3),
-  createData("Décile n°3", "1 760€/mois", 16.0, 24, 6.0),
-  createData("Décile n°4", "2 115€/mois", 3.7, 67, 4.3),
-  createData("Décile n°5", "2 503€/mois", 16.0, 49, 3.9),
-  createData("Décile n°6", "2 941€/mois", 6.0, 24, 4.0),
-  createData("Décile n°7", "3 440€/mois", 9.0, 37, 4.3),
-  createData("Décile n°8", "4 112€/mois", 16.0, 24, 6.0),
-  createData("Décile n°9", "5 267€/mois", 3.7, 67, 4.3),
-  createData("Décile n°10", "∞€/mois", 16.0, 49, 3.9),
+  createData("Décile n°1", "1 135€/mois", 6.0, 6.0, 24, 24, 24, 4.0),
+  createData("Décile n°2", "1 455€/mois", 9.0, 9.0, 37, 37, 37, 4.3),
+  createData("Décile n°3", "1 760€/mois", 16.0, 16.0, 24, 24, 24, 6.0),
+  createData("Décile n°4", "2 115€/mois", 3.7, 3.7, 67, 67, 67, 4.3),
+  createData("Décile n°5", "2 503€/mois", 16.0, 16.0, 49, 49, 49, 3.9),
+  createData("Décile n°6", "2 941€/mois", 6.0, 6.0, 24, 24, 24, 4.0),
+  createData("Décile n°7", "3 440€/mois", 9.0, 9.0, 37, 37, 37, 4.3),
+  createData("Décile n°8", "4 112€/mois", 16.0, 16.0, 24, 24, 24, 6.0),
+  createData("Décile n°9", "5 267€/mois", 3.7, 3.7, 67, 67, 67, 4.3),
+  createData("Décile n°10", "∞€/mois", 16.0, 16.0, 49, 49, 49, 3.9),
 ];
 
 function SimpopTableurInfosDeciles({ classes }) {
   return (
     <Table>
       <TableHead>
-        <TableRow classeName={classes.tableStyle}>
+        <TableRow className={classes.tableStyle}>
           <TableCell padding="dense">Décile</TableCell>
           <TableCell align="center" padding="dense">
             Salaire net /mois (jusqu&apos;à)
@@ -69,7 +76,7 @@ function SimpopTableurInfosDeciles({ classes }) {
       </TableHead>
       <TableBody>
         {rows.map(row => (
-          <TableRow key={row.id} classeName={classes.tableStyle}>
+          <TableRow key={row.id} className={classes.tableStyle}>
             <TableCell component="th" padding="dense" scope="row">
               {row.decile}
             </TableCell>
@@ -77,10 +84,13 @@ function SimpopTableurInfosDeciles({ classes }) {
               {row.salaire}
             </TableCell>
             <TableCell align="center" padding="dense">
-              {row.impotmoyenfoyer}
+              {row.impactmoyenfoyer_plf}
+              &nbsp;{row.impactmoyenfoyer_reforme}
             </TableCell>
             <TableCell align="center" padding="dense">
-              {row.impactmoyenfoyer}
+              {row.impotmoyenfoyer}
+              &nbsp;{row.impotmoyenfoyer_plf}
+              &nbsp;{row.impotmoyenfoyer_reforme}
             </TableCell>
             <TableCell align="center" padding="dense">
               {row.recettesetat}
