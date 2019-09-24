ce0f60bce3e4a1c8034fa041248ac88345563f36
Dorine Lambinet <dorine.lambinet@gmail.com>
2019-09-24 16:20:30 +0200

    Fix Es lint

--- components/carte-etat/simpop-tableur-infos-deciles.jsx
+++ components/carte-etat/simpop-tableur-infos-deciles.jsx
@@ -7,15 +7,13 @@ import TableRow from "@material-ui/core/TableRow";
 import PropTypes from "prop-types";
 
 const styles = {
-  tableStyle: {
-    height: "0px",
-  },
   cellStyle: {
     padding: "0px",
   },
-  reforme: {
-    borderRadius: "0.3em",
-    color: "#00A3FF",
+  codeExistant: {
+    backgroundColor: "#DED500",
+    backgroundSize: "auto auto",
+    color: "#000000",
     fontFamily: "Lato",
     fontSize: "12px",
     fontWeight: "bold",
@@ -34,10 +32,9 @@ const styles = {
     marginRight: "4px",
     padding: "3px",
   },
-  codeExistant: {
-    backgroundColor: "#DED500",
-    backgroundSize: "auto auto",
-    color: "#000000",
+  reforme: {
+    borderRadius: "0.3em",
+    color: "#00A3FF",
     fontFamily: "Lato",
     fontSize: "12px",
     fontWeight: "bold",
@@ -45,6 +42,9 @@ const styles = {
     marginLeft: "4px",
     marginRight: "4px",
     padding: "3px",
+  },
+  tableStyle: {
+    height: "0px",
   },
 };
 
@@ -61,45 +61,63 @@ function createData(
 ) {
   id += 1;
   return {
-    id,
     decile,
-    salaire,
+    id,
     impactmoyenfoyer_plf,
     impactmoyenfoyer_reforme,
     impotmoyenfoyer,
     impotmoyenfoyer_plf,
     impotmoyenfoyer_reforme,
     recettesetat,
+    salaire,
   };
 }
 
 // #décile, salaire net/mois, impact foyer (plf, réforme), impôt moyen foyer (existant, plf, réforme), total recettes état
 // source (2016) http://www.senat.fr/rap/l16-140-211/l16-140-2111.pdf
-const frontdec = [3569,9053,12811,16167,19300,23895,29520,37720,52716, "∞"]
-function create_from_deciles(index, decile){
-    return createData((index + 1),
-      frontdec[index] + "€/an",
-      decile["avant"] ? Math.round((decile["plf"]/decile["avant"] -1)*100) : "N/A",
-      decile["avant"] ? Math.round((decile["apres"]/decile["avant"] -1)*100) : "N/A",
-      Math.round(decile["avant"]/decile["poids"]),
-      Math.round(decile["plf"]/decile["poids"]),
-      Math.round(decile["apres"]/decile["poids"]),
-      Math.round(decile["apres"] / 10000000) / 100 ) // Les recettes de l'Etat doivent être après réforme?
+const frontdec = [
+  3569,
+  9053,
+  12811,
+  16167,
+  19300,
+  23895,
+  29520,
+  37720,
+  52716,
+  "∞",
+];
+function create_from_deciles(index, decile) {
+  return createData(
+    index + 1,
+    `${frontdec[index]}€/an`,
+    decile.avant
+      ? Math.round((decile.plf / decile.avant - 1) * 100)
+      : "N/A",
+    decile.avant
+      ? Math.round((decile.apres / decile.avant - 1) * 100)
+      : "N/A",
+    Math.round(decile.avant / decile.poids),
+    Math.round(decile.plf / decile.poids),
+    Math.round(decile.apres / decile.poids),
+    Math.round(decile.apres / 10000000) / 100,
+  ); // Les recettes de l'Etat doivent être après réforme?
 }
 
 function imageDecile(id) {
   return (
-    <img key={id} alt="" 
-      height="24" width="24"
-      xlinkHref={`/static/images/decile${id}.png`} />
+    <img
+      key={id}
+      alt=""
+      height="24"
+      width="24"
+      xlinkHref={`/static/images/decile${id}.png`}
+    />
   );
 }
 
-
 function SimpopTableurInfosDeciles({ classes, deciles }) {
-  const rows = deciles.map((currElement, index) => {
-    return create_from_deciles(index, currElement);
-  });
+  const rows = deciles.map((currElement, index) => create_from_deciles(index, currElement));
   return (
     <Table>
       <TableHead>
@@ -130,12 +148,15 @@ function SimpopTableurInfosDeciles({ classes, deciles }) {
             </TableCell>
             <TableCell align="center" padding="dense">
               <span style={styles.plf}>{row.impactmoyenfoyer_plf}</span>
-              &nbsp;<span style={styles.reforme}>{row.impactmoyenfoyer_reforme}</span>
+              &nbsp;
+              <span style={styles.reforme}>{row.impactmoyenfoyer_reforme}</span>
             </TableCell>
             <TableCell align="center" padding="dense">
               <span style={styles.codeExistant}>{row.impotmoyenfoyer}</span>
-              &nbsp;<span style={styles.plf}>{row.impotmoyenfoyer_plf}</span>
-              &nbsp;<span style={styles.reforme}>{row.impotmoyenfoyer_reforme}</span>
+              &nbsp;
+              <span style={styles.plf}>{row.impotmoyenfoyer_plf}</span>
+              &nbsp;
+              <span style={styles.reforme}>{row.impotmoyenfoyer_reforme}</span>
             </TableCell>
             <TableCell align="center" padding="dense">
               {row.recettesetat}
