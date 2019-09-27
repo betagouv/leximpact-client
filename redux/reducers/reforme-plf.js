265754fc1162ed9a76f0a9bc726b5b8cb79c61d2
Augustin Wenger <magemax@yahoo.com>
2019-09-27 17:41:52 +0200

    update base values and reform content

--- redux/reducers/reforme-plf.js
+++ redux/reducers/reforme-plf.js
@@ -1,13 +1,13 @@
 export const REFORME_PLF_DEFAULT_STATE = {
   impot_revenu: {
     bareme: {
-      seuils: [9964, 25405, 72643, 156244],
+      seuils: [10064, 25659, 73369, 157806],
       taux: [0.11, 0.3, 0.41, 0.45],
     },
     decote: {
-      seuil_celib: 768,
-      seuil_couple: 1272,
-      taux: 0.45,
+      seuil_celib: 777,
+      seuil_couple: 1286,
+      taux: 0.4525,
     },
     plafond_qf: {
       abat_dom: {
@@ -16,14 +16,14 @@ export const REFORME_PLF_DEFAULT_STATE = {
         taux_GuadMarReu: 0.3,
         taux_GuyMay: 0.4,
       },
-      celib: 927,
-      celib_enf: 3660,
-      maries_ou_pacses: 1551,
-      reduc_postplafond: 1547,
-      reduc_postplafond_veuf: 1728,
+      celib: 936,
+      celib_enf: 3697,
+      maries_ou_pacses: 1567,
+      reduc_postplafond: 1562,
+      reduc_postplafond_veuf: 1745,
       reduction_ss_condition_revenus: {
-        seuil1: 18984,
-        seuil2: 21036,
+        seuil1: 18985,
+        seuil2: 21037,
         seuil_maj_enf: 3797,
         taux: 0,
       },
