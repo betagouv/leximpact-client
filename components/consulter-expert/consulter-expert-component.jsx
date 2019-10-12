7262326eaa0ccc04de1d17aed061a3d31b23016c
Dorine Lambinet <dorine.lambinet@gmail.com>
2019-10-12 17:32:08 +0200

    Ajout du texte et d'un en savoir plus

--- components/consulter-expert/consulter-expert-component.jsx
+++ components/consulter-expert/consulter-expert-component.jsx
@@ -8,8 +8,12 @@ import {
   IconButton,
   Typography,
 } from "@material-ui/core";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import { withStyles } from "@material-ui/core/styles";
 import { Close as CloseIcon } from "@material-ui/icons";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import PropTypes from "prop-types";
 import React, { PureComponent } from "react";
 
@@ -55,6 +59,19 @@ const styles = () => ({
     fontSize: "0.875em",
     marginLeft: "10px",
     textJustify: "left",
+    marginBottom: "0.7em",
+  },
+  styleExpansionPanel: {
+    backgroundColor: "rgba(222, 213, 0, 0)",
+    boxShadow: "none",
+    padding: "0px",
+  },
+
+  subtitleEnSavoirPlus: {
+    color: "#565656",
+    fontFamily: "Lato",
+    fontSize: "0.875em",
+    padding: "0 0 10px 10px",
   },
   titleCard: {
     color: "#565656",
@@ -68,7 +85,12 @@ const styles = () => ({
 
 class ConsulterExpertCard extends PureComponent {
   render() {
-    const { classes, onRemoveConsulterExpert } = this.props;
+    const {
+      classes,
+      expandArticlePanelHandler,
+      isPanelExpanded,
+      onRemoveConsulterExpert,
+    } = this.props;
 
     return (
       <Card className={classes.cardContainer}>
@@ -79,20 +101,43 @@ class ConsulterExpertCard extends PureComponent {
             </div>
             <div className={classes.divTitre}>
               <Typography className={classes.titleCard}>
-                Comment utiliser LexImpact ?
+                Ce que calcule LexImpact
               </Typography>
               <Typography className={classes.subtitleCard}>
-                LexImpact présente les impacts d&apos;une modification de la
-                loi,
+                LexImpact fonctionne à euros courants,
                 {" "}
-                <b>ce simulateur fonctionne donc à euros courants</b>
-.
-                {" "}
+                <b>
+                  l&apos;inflation n&apos;est pas paramétrée dans le simulateur.
+                </b>
                 <br />
-                Si vous souhaitez prendre en compte des hypothèses (telles que
-                l&apos;inflation), il faut les ajouter dans votre simulation ou
-                consulter un expert LexImpact.
               </Typography>
+
+              <ExpansionPanel
+                classes={{ root: classes.styleExpansionPanel }}
+                expanded={isPanelExpanded}
+                onChange={expandArticlePanelHandler}>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography className={classes.subtitleEnSavoirPlus}>
+                    En savoir plus
+                  </Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <Typography className={classes.subtitleEnSavoirPlus}>
+                    La comparaison est effectuée entre le code existant et le
+                    PLF prévu. Or, ces deux codes ne s&apos;appliquent pas aux
+                    mêmes revenus. Afin d&apos;éviter qu&apos;une hausse de
+                    revenu consécutive à l&apos;inflation ne se manifeste par
+                    une hausse du taux d&apos;imposition, le PLF revalorise
+                    souvent les seuils et les plafonds d&apos;un taux
+                    correspondant au taux d&apos;inflation. La comparaison
+                    effectuée par Leximpact est centrée sur les impacts de la
+                    loi, et considère donc un ménage dont le revenu nominal ne
+                    change pas entre le code existant et le PLF, ce qui, si on
+                    considère l&apos;inflation, signifierait une baisse de leur
+                    revenu réel.
+                  </Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
             </div>
 
             <div className={classes.cardHeaderButtons}>
@@ -113,6 +158,8 @@ class ConsulterExpertCard extends PureComponent {
 ConsulterExpertCard.propTypes = {
   classes: PropTypes.shape().isRequired,
   onRemoveConsulterExpert: PropTypes.func.isRequired,
+  isPanelExpanded: PropTypes.shape().isRequired,
+  expandArticlePanelHandler: PropTypes.shape().isRequired,
 };
 
 export default withStyles(styles)(ConsulterExpertCard);
