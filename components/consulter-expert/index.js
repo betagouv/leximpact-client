8fa71ffca57d83fd17fe519de27fb53c051d337f
sandcha <sandcha@users.noreply.github.com>
2019-10-28 15:23:00 +0100

    Corrige les warnings de console sur ConsulterExpertCard

--- components/consulter-expert/index.js
+++ components/consulter-expert/index.js
@@ -1,12 +1,20 @@
 import { connect } from "react-redux";
 
-import { closeConsulterExpert } from "../../redux/actions";
+import { closeConsulterExpert, expandArticlePanel } from "../../redux/actions";
 import ConsulterExpertCard from "./consulter-expert-component";
 
-const mapStateToProps = null;
+const PANEL_NAME = "consulter_expert";
+
+const mapStateToProps  = ({ currentExpandedArticlePanel }) => {
+  const isPanelExpanded = currentExpandedArticlePanel === PANEL_NAME;
+  return {
+    isPanelExpanded,
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   onRemoveConsulterExpert: () => dispatch(closeConsulterExpert()),
+  onExpandPanel: () => dispatch(expandArticlePanel(PANEL_NAME)),
 });
 
 export default connect(
