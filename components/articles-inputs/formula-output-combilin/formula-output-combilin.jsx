7d1d97b11d4c2dfbcb8be31aa38552d2eda4e256
Loïc Poullain <loic.poullain@centraliens.net>
2020-03-11 15:47:06 +0100

    Rendre plfValue optionnel dans FormlaOtputCombiLin

--- components/articles-inputs/formula-output-combilin/formula-output-combilin.jsx
+++ components/articles-inputs/formula-output-combilin/formula-output-combilin.jsx
@@ -1,23 +1,38 @@
 import PropTypes from "prop-types";
 import { Fragment } from "react";
 
+
 import formatMilliers from "../../utils/format-milliers";
+import makeNumberGoodLooking from "../../articles/utils/make-number-good-looking";
 import OutputField from "../output-field";
+
+function formatNumber(n) {
+  return formatMilliers(makeNumberGoodLooking(n));
+}
 
 const FormulaOutputCombiLin = ({
   baseValue, newValue, plfValue, style,
-}) => (
-  <Fragment>
-    <OutputField style={Math.abs(baseValue - plfValue) < 0.001 ? style.VarCodeexistantNonBarre : style.VarCodeexistant} value={formatMilliers(baseValue)} />
-    {Math.abs(baseValue - plfValue) < 0.001 ? "" : <OutputField style={style.VarPLF} value={formatMilliers(plfValue)} />}
-    <OutputField style={style.VarCodeNew} value={formatMilliers(newValue)} />
-  </Fragment>
-);
+}) => {
+  const montrerPLF = plfValue !== null && plfValue !== baseValue;
+  return (
+    <Fragment>
+      <OutputField
+        style={montrerPLF ? style.VarCodeexistant : style.VarCodeexistantNonBarre}
+        value={formatNumber(baseValue)} />
+      {montrerPLF && <OutputField style={style.VarPLF} value={formatNumber(plfValue)} />}
+      <OutputField style={style.VarCodeNew} value={formatNumber(newValue)} />
+    </Fragment>
+  );
+};
+
+FormulaOutputCombiLin.defaultProps = {
+  plfValue: null,
+};
 
 FormulaOutputCombiLin.propTypes = {
-  baseValue: PropTypes.string.isRequired,
-  newValue: PropTypes.string.isRequired,
-  plfValue: PropTypes.string.isRequired,
+  baseValue: PropTypes.number.isRequired,
+  newValue: PropTypes.number.isRequired,
+  plfValue: PropTypes.number,
   style: PropTypes.shape().isRequired,
 };
 
