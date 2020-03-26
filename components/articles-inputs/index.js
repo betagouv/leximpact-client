a544a193489718d5a4dcbff0b4fe469e4bbdc458
Loïc Poullain <loic.poullain@centraliens.net>
2020-03-26 11:00:49 +0100

    Delete unused component

--- components/articles-inputs/index.js
+++ components/articles-inputs/index.js
@@ -1,5 +1,4 @@
 export { Parameter } from "./parameter";
-export { default as BaseOutput } from "./base-output";
 export { default as BaseInputOutput } from "./base-input-output";
 export { default as FormulaOutput } from "./formula-output";
 export { default as FormulaOutputCombilin } from "./formula-output-combilin";
