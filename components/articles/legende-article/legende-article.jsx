cfceb139f94f35f1f01b434ac6b12e9640cddfde
Loïc Poullain <loic.poullain@centraliens.net>
2020-04-14 15:57:52 +0200

    Update LegendeArticle style

--- components/articles/legende-article/legende-article.jsx
+++ components/articles/legende-article/legende-article.jsx
@@ -1,107 +1,31 @@
-import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 import OpenInNewIcon from "@material-ui/icons/OpenInNew";
 import PropTypes from "prop-types";
 
-const stylesTheme = theme => ({
-  alink: {
-    "&:hover": {
-      color: "#00A3FF",
-      textDecoration: "underline",
-    },
-    color: "#FF6B6B",
-    textDecoration: "none",
-  },
-  container: {
-    margin: "2em 1em 0 1em",
-    padding: "0",
-    paddingBottom: "7px",
-    [theme.breakpoints.down("xs")]: {
-      margin: "0em",
-    },
-  },
-  styleAmendement: {
-    borderRadius: "0.3em",
-    color: "#00A3FF",
-    fontFamily: "Lato",
-    fontSize: "12px",
-    fontWeight: "bold",
-    lineHeight: "10px",
-    marginLeft: "4px",
-    marginRight: "4px",
-    padding: "3px",
-  },
-  styleAvantPLF: {
-    color: "#FF6B6B",
-    fontFamily: "Lato",
-    fontSize: "12px",
-    fontWeight: "bold",
-    lineHeight: "10px",
-    marginLeft: "4px",
-    marginRight: "4px",
-    padding: "3px",
-  },
-  styleCodeExistant: {
-    backgroundColor: "#DED500",
-    backgroundSize: "auto auto",
-    color: "#000000",
-    fontFamily: "Lato",
-    fontSize: "12px",
-    fontWeight: "bold",
-    lineHeight: "10px",
-    marginLeft: "4px",
-    marginRight: "4px",
-    padding: "3px",
-  },
-  styleLegende: {
-    color: "#565656",
-    fontFamily: "Lato",
-    fontSize: "12px",
-    fontWeight: "regular",
-    lineHeight: "10px",
-    marginLeft: "0px",
-    marginRight: "8px",
-    padding: "0 3px 0 0",
-  },
-  styleOpenInNewIcon: {
-    color: "#FF6B6B",
-    fontFamily: "Lato",
-    fontSize: "13px",
-    fontWeight: "bold",
-    lineHeight: "10px",
-    texAlign: "center",
-  },
-});
+import styles from "./legende-article.module.scss";
 
-function LegendeArticle({ classes, montrerPLF }) {
+function LegendeArticle({ montrerPLF }) {
   return (
-    <div className={classes.container}>
-      <Typography inline className={classes.styleLegende}>
-        Légende&nbsp;:
-      </Typography>
-      <Typography inline className={classes.styleCodeExistant}>
-        Droit existant
-      </Typography>
-      {montrerPLF && (
-        <Typography inline className={classes.styleAvantPLF}>
-          <a
-            className={classes.alink}
-            href="http://www.assemblee-nationale.fr/15/projets/pl2272.asp"
-            target="blank">
-            PLF 2020
-            <OpenInNewIcon className={classes.styleOpenInNewIcon} />
-          </a>
-        </Typography>
-      )}
-      <Typography inline className={classes.styleAmendement}>
-        Mon amendement
-      </Typography>
+    <div>
+      <div className={styles.legend}>
+        Légende :
+      </div>
+      <div>
+        <span className={styles.initial}>Droit existant</span>
+        {montrerPLF && (
+          <span className={styles.plf}>
+            <a href="http://www.assemblee-nationale.fr/15/projets/pl2272.asp">
+              PLF 2020
+              <OpenInNewIcon className={styles.icon} />
+            </a>
+          </span>
+        )}
+        <span className={styles.amendment}>Mon amendment</span>
+      </div>
     </div>
   );
 }
 
 LegendeArticle.propTypes = {
-  classes: PropTypes.shape().isRequired,
   montrerPLF: PropTypes.bool.isRequired,
 };
-export default withStyles(stylesTheme, { withTheme: true })(LegendeArticle);
+export default LegendeArticle;
