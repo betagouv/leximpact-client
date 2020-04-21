85dced4a01eaec455e55bfd41f0132f1a07ecdb9
Loïc Poullain <loic.poullain@centraliens.net>
2020-04-21 17:26:03 +0200

    Make all rates be integers.

--- redux/actions/format-reforme.js
+++ redux/actions/format-reforme.js
@@ -1,16 +1,20 @@
+import merge from "lodash/merge";
+
 export function formatReforme(reforme) {
-  return {
-    ...reforme,
+  return merge({}, reforme, {
     impot_revenu: {
-      ...reforme.impot_revenu,
       bareme: {
-        ...reforme.impot_revenu.bareme,
         taux: reforme.impot_revenu.bareme.taux.map(taux => taux / 100),
       },
       decote: {
-        ...reforme.impot_revenu.decote,
         taux: reforme.impot_revenu.decote.taux / 100,
       },
+      plafond_qf: {
+        abat_dom: {
+          taux_GuadMarReu: reforme.impot_revenu.plafond_qf.abat_dom.taux_GuadMarReu / 100,
+          taux_GuyMay: reforme.impot_revenu.plafond_qf.abat_dom.taux_GuyMay / 100,
+        },
+      },
     },
-  };
+  });
 }
