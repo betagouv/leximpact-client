c85cca41a838974f61e87d7d63bcb4c2340f0bec
Loïc Poullain <loic.poullain@centraliens.net>
2020-04-22 11:29:26 +0200

    Remove warning on <Container>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
 import withRedux from "next-redux-wrapper";
-import App, { Container } from "next/app";
+import App from "next/app";
 import React from "react";
 import { Provider } from "react-redux";
 
@@ -9,11 +9,9 @@ class LexImpactApplicationWrapper extends App {
   render() {
     const { Component, pageProps, store } = this.props;
     return (
-      <Container>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
-      </Container>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     );
   }
 }
