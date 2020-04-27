da2e86a8a3f7ef7d3b3a0778e20cbf57b44b70da
Loïc Poullain <loic.poullain@centraliens.net>
2020-04-27 09:38:42 +0200

    Fix conflicts

--- components/simulation-menu/legende/Legende.jsx
+++ components/simulation-menu/legende/Legende.jsx
@@ -19,7 +19,7 @@ function Legende({ montrerPLF }) {
             </a>
           </span>
         )}
-        <span className={styles.amendment}>Mon amendment</span>
+        <span className={styles.reform}>Mon amendment</span>
       </div>
     </div>
   );
