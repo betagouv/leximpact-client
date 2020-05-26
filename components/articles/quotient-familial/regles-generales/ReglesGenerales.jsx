0ad4801c5f1afbf81cc16912d84191e24066c288
sandcha <sandcha@users.noreply.github.com>
2020-05-26 21:06:12 +0200

    Fix linting

--- components/articles/quotient-familial/regles-generales/ReglesGenerales.jsx
+++ components/articles/quotient-familial/regles-generales/ReglesGenerales.jsx
@@ -6,7 +6,8 @@ export class ReglesGenerales extends PureComponent {
   render() {
     return (
       <div className={styles.text}>
-        I. Le nombre de parts à prendre en considération pour la division du revenu imposable prévue à l'article 193 est déterminé conformément aux dispositions suivantes :
+        I. Le nombre de parts à prendre en considération pour la division du revenu imposable
+        prévue à l&apos;article 193 est déterminé conformément aux dispositions suivantes :
         <br />
         [Tableau]
       </div>
