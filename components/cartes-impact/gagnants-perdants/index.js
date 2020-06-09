bf950599ec0ed2b6c811baa58eeae00cfd642425
sandcha <sandcha@users.noreply.github.com>
2020-06-09 11:31:03 +0200

    Fix imports as detected by linter

--- components/cartes-impact/gagnants-perdants/index.js
+++ components/cartes-impact/gagnants-perdants/index.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 
-import { fetchCalculateCompare, fetchSimPop } from "../../../redux/actions";
+import { fetchSimPop, simulateCasTypes } from "../../../redux/actions";
 import GagnantsPerdantsCard from "./gagnants-perdants-component";
 
 const mapStateToProps = ({ loadingEtat, totalPop }) => {
