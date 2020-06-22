e2d57365f9e467b6b8aab3512153432bad1edc0e
Loïc Poullain <loic.poullain@centraliens.net>
2020-06-22 15:28:07 +0200

    Add empty SearchInput

--- components/dotations/results/commune-search/CommuneSearch.tsx
+++ components/dotations/results/commune-search/CommuneSearch.tsx
@@ -1,16 +1,13 @@
 import { PureComponent } from "react";
 
 import { Card } from "../../../card";
+import { SearchInput } from "./search-input";
 
 export class CommuneSearch extends PureComponent {
   render() {
     return (
       <Card
-        content1={(
-          <div>
-            Barre de recherche
-          </div>
-        )}
+        content1={<SearchInput />}
         title="Ajouter une nouvelle commune"
       />
     );
---
+++ components/dotations/results/commune-search/search-input/SearchInput.tsx
@@ -0,0 +1,9 @@
+import { PureComponent } from "react";
+
+export class SearchInput extends PureComponent {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
