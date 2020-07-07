39fa2a577b195ac2a39c2c371be06d3748f4e5db
Loïc Poullain <loic.poullain@centraliens.net>
2020-07-07 10:36:18 +0200

    Add dotations data

--- redux/reducers/parameters/base/dotations.ts
+++ redux/reducers/parameters/base/dotations.ts
@@ -1,6 +1,79 @@
+/* eslint-disable sort-keys */
+
 // eslint-disable-next-line no-unused-vars
 import { DotationsState } from "../interfaces";
 
-export const BASE_DOTATIONS_DEFAULT_STATE: DotationsState = {};
+export const BASE_DOTATIONS_DEFAULT_STATE: DotationsState = {
+  // Article L1613-1 du CGCT
+  montants: {
+    dgf: 26846874416,
+  },
+  communes: {
+    dsr: {
+      // Article L2334-20 du CGCT
+      eligibilite: {
+        popChefLieuMax: 20000,
+        popMax: 10000,
+      },
+      // Article L2334-21  du CGCT
+      bourgCentre: {
+        eligibilite: {
+          partPopCantonMin: 15,
+          exclusion: {
+            agglomeration: {
+              partPopDepartementMin: 10,
+              popMin: 250000,
+              popCommuneMin: 100000,
+            },
+            canton: {
+              popChefLieuMin: 10000,
+            },
+            potentielFinancier: {
+              rapportPotentielFinancierMoyen: 2,
+            },
+          },
+        },
+        attribution: {
+          popLimite: 10000,
+          effortFiscalLimite: 1.2,
+          coefMultiplicateurRevitalisationRurale: 1.3,
+          pourcentageAttributionMin: 90,
+          pourcentageAttributionMax: 120,
+          // plafonnementPopulation: {
+          //   500: 100,
+          //   1000: 500,
+          //   2250: 1500,
+          // },
+        },
+      },
+      // Article L2334-22 du CGCT
+      perequation: {
+        eligibilite: {
+          rapportPotentielFinancier: 2,
+        },
+        attribution: {
+          repartition: {
+            ponderationPotentielFinancier: 30,
+            ponderationLongueurVoirie: 30,
+            ponderationNbreEnfants: 30,
+            ponderationPotentielFinancierParHectare: 10,
+          },
+          pourcentageAttributionMin: 90,
+          pourcentageAttributionMax: 120,
+        },
+      },
+      // Article L2334-22-1 du CGCT
+      cible: {
+        eligibilite: {
+          premieresCommunes: 10000,
+          indiceSynthetique: {
+            ponderationPotentielFinancier: 70,
+            ponderationRevenu: 30,
+          },
+        },
+      },
+    },
+  },
+};
 
 export const dotations = (state = BASE_DOTATIONS_DEFAULT_STATE) => state;
--- redux/reducers/parameters/interfaces/dotations-state.ts
+++ redux/reducers/parameters/interfaces/dotations-state.ts
@@ -1,1 +1,70 @@
-export interface DotationsState {}
+export interface DotationsState {
+  // Article L1613-1 du CGCT
+  montants: {
+    dgf: number;
+  }
+  communes: {
+    dsr: {
+      // Article L2334-20 du CGCT
+      eligibilite: {
+        popMax: number;
+        popChefLieuMax: number;
+      }
+      // Article L2334-21  du CGCT
+      bourgCentre: {
+        eligibilite: {
+          partPopCantonMin: number;
+          exclusion: {
+            agglomeration: {
+              partPopDepartementMin: number;
+              popMin: number;
+              popCommuneMin: number;
+            }
+            canton: {
+              popChefLieuMin: number;
+            }
+            potentielFinancier: {
+              rapportPotentielFinancierMoyen: number;
+            }
+          }
+        }
+        attribution: {
+          popLimite: number;
+          effortFiscalLimite: number;
+          coefMultiplicateurRevitalisationRurale: number;
+          pourcentageAttributionMin: number;
+          pourcentageAttributionMax: number;
+          // plafonnementPopulation: {
+          //   [recensement: number]: number;
+          // }
+        }
+      }
+      // Article L2334-22 du CGCT
+      perequation: {
+        eligibilite: {
+          rapportPotentielFinancier: number;
+        }
+        attribution: {
+          repartition: {
+            ponderationPotentielFinancier: number;
+            ponderationLongueurVoirie: number;
+            ponderationNbreEnfants: number;
+            ponderationPotentielFinancierParHectare: number;
+          }
+          pourcentageAttributionMin: number;
+          pourcentageAttributionMax: number;
+        }
+      }
+      // Article L2334-22-1 du CGCT
+      cible: {
+        eligibilite: {
+          premieresCommunes: number;
+          indiceSynthetique: {
+            ponderationPotentielFinancier: number;
+            ponderationRevenu: number;
+          }
+        }
+      }
+    }
+  }
+}
