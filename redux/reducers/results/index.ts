cedb74cb3cebe4d6a2d4a6124d6e1dbff19b4e06
Loïc Poullain <loic.poullain@centraliens.net>
2020-07-20 14:22:43 +0200

    Fix bad merge

--- redux/reducers/results/index.ts
+++ redux/reducers/results/index.ts
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
 
+import { amendement } from "./amendement";
+import { base } from "./base";
+import { baseToAmendement } from "./baseToAmendement";
+import { baseToPlf } from "./baseToPlf";
 import casTypes from "./cas-types";
+import { plf } from "./plf";
 import totalPop from "./total-pop";
 
 export default combineReducers({
+  amendement,
+  base,
+  baseToAmendement,
+  baseToPlf,
+  plf,
   casTypes,
   totalPop,
 });
