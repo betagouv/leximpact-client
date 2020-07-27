b92b2e1d8ec7abbbfcb6b455249afcdf78bc0777
Loïc Poullain <loic.poullain@centraliens.net>
2020-07-27 17:05:43 +0200

    Hide dotations button

--- components/home/home-content/home-navigation/HomeNavigation.tsx
+++ components/home/home-content/home-navigation/HomeNavigation.tsx
@@ -13,13 +13,13 @@ export class HomeNavigation extends PureComponent {
           d&apos;une modification de la loi :
         </h1>
         <button type="button" onClick={() => Router.push("/ir")}>
-          <img src="/icons/picto-impot-sur-le-revenu.png" alt=""/>
+          <img alt="" src="/icons/picto-impot-sur-le-revenu.png" />
           sur l&apos;impôt sur le revenu
         </button>
-        <button type="button" onClick={() => Router.push("/dotations")}>
-          <img src="/icons/picto-dotations-communes.png" alt=""/>
+        {/* <button type="button" onClick={() => Router.push("/dotations")}>
+          <img alt="" src="/icons/picto-dotations-communes.png" />
           sur les dotations aux collectivités territoriales
-        </button>
+        </button> */}
       </div>
     );
   }
