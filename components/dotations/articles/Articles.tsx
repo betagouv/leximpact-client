800d83281c63b017e762f00ba2b095a5bbad0124
Loïc Poullain <loic.poullain@centraliens.net>
2020-08-03 10:02:37 +0200

    Fix linting

--- components/dotations/articles/Articles.tsx
+++ components/dotations/articles/Articles.tsx
@@ -8,14 +8,14 @@ import { DsrFractionPerequation } from "./dsr-fraction-perequation";
 import { DsuEligibilite } from "./dsu-eligibilite";
 import { DsuIndice } from "./dsu-indice";
 import { DsuRepartition } from "./dsu-repartition";
-import { MontantDgf } from "./montant-dgf";
+// import { MontantDgf } from "./montant-dgf";
 // import { MontantDgfCommunes } from "./montant-dgf-communes";
 import { MontantDsrDsu } from "./montant-dsr-dsu";
 
 export class Articles extends PureComponent {
   render() {
     const url = new URLSearchParams(window.location.search);
-    const isDsuVisible = url.has('dsu');
+    const isDsuVisible = url.has("dsu");
     return (
       <Fragment>
         {/* Article header */}
@@ -45,43 +45,47 @@ export class Articles extends PureComponent {
               <DsrFractionCible />
             </SecondaryExpandablePanel>
           </PrimaryExpandablePanel>
-          {isDsuVisible && <PrimaryExpandablePanel
-            title="Dotation de solidarité urbaine (DSU)">
-            <SecondaryExpandablePanel
-              subTitle="Article L2334-16 du CGCT"
-              title="Périmètre général d&apos;éligibilité">
-              <DsuEligibilite />
-            </SecondaryExpandablePanel>
-            <SecondaryExpandablePanel
-              subTitle="Article L2334-18-2 du CGCT"
-              title="Répartition">
-              <DsuRepartition />
-            </SecondaryExpandablePanel>
-            <SecondaryExpandablePanel
-              subTitle="Article L2334-17 du CGCT"
-              title="Définition de l’indice synthétique">
-              <DsuIndice />
-            </SecondaryExpandablePanel>
-          </PrimaryExpandablePanel>}
-          {isDsuVisible && <PrimaryExpandablePanel
-            title="Montant des dotations">
-            {/* <SecondaryExpandablePanel
+          {isDsuVisible && (
+            <PrimaryExpandablePanel
+              title="Dotation de solidarité urbaine (DSU)">
+              <SecondaryExpandablePanel
+                subTitle="Article L2334-16 du CGCT"
+                title="Périmètre général d&apos;éligibilité">
+                <DsuEligibilite />
+              </SecondaryExpandablePanel>
+              <SecondaryExpandablePanel
+                subTitle="Article L2334-18-2 du CGCT"
+                title="Répartition">
+                <DsuRepartition />
+              </SecondaryExpandablePanel>
+              <SecondaryExpandablePanel
+                subTitle="Article L2334-17 du CGCT"
+                title="Définition de l’indice synthétique">
+                <DsuIndice />
+              </SecondaryExpandablePanel>
+            </PrimaryExpandablePanel>
+          )}
+          {isDsuVisible && (
+            <PrimaryExpandablePanel
+              title="Montant des dotations">
+              {/* <SecondaryExpandablePanel
               expanded
               subTitle="Article L1613-1 du CGCT"
               title="Montant de la dotation globale de fonctionnement (DGF)">
               <MontantDgf />
             </SecondaryExpandablePanel> */}
-            {/* <SecondaryExpandablePanel
+              {/* <SecondaryExpandablePanel
               subTitle="Article L2334-1"
               title="Montant de la DGF communes">
               <MontantDgfCommunes />
             </SecondaryExpandablePanel> */}
-            <SecondaryExpandablePanel
-              subTitle="Article L2334-13 du CGCT"
-              title="Montants de la DSR et de la DSU">
-              <MontantDsrDsu />
-            </SecondaryExpandablePanel>
-          </PrimaryExpandablePanel>}
+              <SecondaryExpandablePanel
+                subTitle="Article L2334-13 du CGCT"
+                title="Montants de la DSR et de la DSU">
+                <MontantDsrDsu />
+              </SecondaryExpandablePanel>
+            </PrimaryExpandablePanel>
+          )}
         </div>
       </Fragment>
     );
