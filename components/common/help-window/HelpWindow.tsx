25a5beeee959343bac1e18bce72a66dc695ede57
Loïc Poullain <loic.poullain@centraliens.net>
2020-08-25 18:14:25 +0200

    Implement HelpWindow

--- components/common/help-window/HelpWindow.tsx
+++ components/common/help-window/HelpWindow.tsx
@@ -1,13 +1,16 @@
+import CloseIcon from "@material-ui/icons/Close";
 import { PureComponent } from "react";
 // eslint-disable-next-line no-unused-vars
 import { connect, ConnectedProps } from "react-redux";
 
+import { hideHelpWindow } from "../../../redux/actions";
 // eslint-disable-next-line no-unused-vars
 import { RootState } from "../../../redux/reducers";
 import styles from "./HelpWindow.module.scss";
 
 interface Props {
   name: string;
+  title: string;
   children: JSX.Element;
 }
 
@@ -15,19 +18,37 @@ const mapStateToProps = ({ display }: RootState, { name }: Props) => ({
   isVisible: display.currentHelpWindow === name,
 });
 
-const connector = connect(mapStateToProps);
+const mapDispatchToProps = dispatch => ({
+  close: () => dispatch(hideHelpWindow()),
+});
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
 
 type PropsFromRedux = ConnectedProps<typeof connector>
 
 class HelpWindow extends PureComponent<PropsFromRedux & Props> {
   render() {
-    const { children, isVisible } = this.props;
+    const {
+      children, close, isVisible, title,
+    } = this.props;
 
     if (!isVisible) {
       return null;
     }
 
-    return <div className={styles.container}>{children}</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.window}>
+          <div className={styles.header}>
+            <span>{title}</span>
+            <CloseIcon className={styles.close} fontSize="large" onClick={close} />
+          </div>
+          <div className={styles.content}>
+            {children}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
