1d5a4e7f6b98131ebcb7bcd9f8e2d37081ada045
Loïc Poullain <loic.poullain@centraliens.net>
2020-09-09 11:09:31 +0200

    Fix CommuneSearch "enter" bug

--- components/dotations/results/commune-search/search-input/SearchInput.tsx
+++ components/dotations/results/commune-search/search-input/SearchInput.tsx
@@ -2,105 +2,106 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import TextField from "@material-ui/core/TextField";
 import SearchIcon from "@material-ui/icons/Search";
-import Autocomplete from "@material-ui/lab/Autocomplete";
 import debounce from "lodash/debounce";
-import { PureComponent } from "react";
+import { createRef, Fragment, PureComponent } from "react";
 
 // eslint-disable-next-line no-unused-vars
 import { Commune } from "../../../../../redux/reducers/descriptions/dotations";
 import request from "../../../../common/utils/request";
+import { SearchList } from "./search-list";
 
 interface Props {
   onChange: (commune: Commune) => void;
 }
 
 interface State {
-  open: boolean;
   communes: Commune[];
-  value: Commune | null;
+  value: string;
   loading: boolean;
 }
 
 type ResponseBody = Commune[];
 
 export class SearchInput extends PureComponent<Props, State> {
+  inputRef = createRef();
+
   constructor(props) {
     super(props);
     this.state = {
       communes: [],
       loading: false,
-      open: false,
-      value: null,
+      value: "",
     };
-    this.search = debounce(this.search.bind(this), 500);
+
+    this.search = this.search.bind(this);
+    this.fetchCommunes = debounce(this.fetchCommunes.bind(this), 500);
+    this.select = this.select.bind(this);
   }
 
-  // eslint-disable-next-line no-unused-vars
-  async search(search: string): Promise<void> {
-    this.setState({ communes: [] });
+  search(value: string): void {
+    this.setState({
+      communes: [],
+      value,
+    });
 
-    if (!search || search.length < 3) {
+    this.fetchCommunes(value);
+  }
+
+  async fetchCommunes(value: string): Promise<void> {
+    if (value.length < 3) {
       return;
     }
 
     this.setState({ loading: true });
 
     await request
-      .get(`/search?commune=${encodeURI(search)}`)
+      .get(`/search?commune=${encodeURI(value)}`)
       .then((communes: ResponseBody) => {
         this.setState({
           communes,
           loading: false,
         });
       })
-      .catch(() => {});
+      .catch(() => { });
+  }
+
+  select(commune: Commune): void {
+    this.setState({
+      communes: [],
+      value: "",
+    });
+
+    // eslint-disable-next-line react/destructuring-assignment
+    this.props.onChange(commune);
   }
 
   render() {
-    const { onChange } = this.props;
     const {
-      communes, loading, open, value,
+      communes, loading, value,
     } = this.state;
     return (
-      <Autocomplete
-        freeSolo
-        getOptionLabel={option => `${option.name} (${option.code})`}
-        open={open}
-        options={communes}
-        renderInput={params => (
-          <TextField
-            {...params}
-            color="secondary"
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  {loading && <CircularProgress color="secondary" size={20} style={{ marginRight: 10 }} />}
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-              ref: params.InputProps.ref,
-            }}
-            label="Nom de la commune"
-            margin="normal"
-            variant="filled"
-          />
-        )}
-        style={{ width: "100%" }}
-        value={value}
-        onChange={(event: any, commune: Commune | null) => {
-          if (commune) {
-            onChange(commune);
-            this.setState({ communes: [], value: commune });
-            // Small hack to clear the input.
-            setTimeout(() => this.setState({ value: null }), 0);
-          }
-        }}
-        onClose={() => this.setState({ open: false })}
-        onInputChange={(event, search) => {
-          this.search(search);
-        }}
-        onOpen={() => this.setState({ open: true })}
-      />
+      <Fragment>
+        <TextField
+          fullWidth
+          color="secondary"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                {loading && <CircularProgress color="secondary" size={20} style={{ marginRight: 10 }} />}
+                <SearchIcon />
+              </InputAdornment>
+            ),
+            ref: this.inputRef,
+          }}
+          label="Nom de la commune"
+          margin="normal"
+          value={value}
+          variant="filled"
+          onChange={e => this.search(e.target.value)}
+          onKeyDown={e => e.key === "Enter" && this.fetchCommunes(value)}
+        />
+        {communes.length !== 0 && <SearchList communes={communes} onSelect={this.select} />}
+      </Fragment>
     );
   }
 }
